afab59c8d279a392554352c9ce4fe3a0cd4dfb19
YSXCC <46277907+YSXCC@users.noreply.github.com>
2020-12-10 13:01:58 +0800

    Site updated: 2020-12-10 13:01:50

--- js/duration.js
+++ js/duration.js
@@ -31,7 +31,7 @@
       if(String(snum).length === 1 ){
         snum = "0" + snum;
       }
-      document.getElementById("timeDate").innerHTML = "WeTree已经成长了&nbsp"+dnum+"&nbsp天";
+      document.getElementById("timeDate").innerHTML = "神明已经消失了:&nbsp"+dnum+"&nbsp天";
       document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
     }
     setInterval(update, 1000);
